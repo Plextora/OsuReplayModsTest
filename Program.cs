bc942f79ac5c21821c937871f9daa84f0aa3b192
Plextora <71889427+Plextora@users.noreply.github.com>
2023-03-11 14:57:43 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using OsuParsers.Decoders;
+using OsuParsers.Enums;
+
+var osuReplay = ReplayDecoder.Decode(@"C:\Users\Plextora\Desktop\test.osr"); // osr to edit
+var mods = new List<Mods> { Mods.DoubleTime, Mods.Flashlight }; // list of mods to add for replay
+
+osuReplay.Mods = (int)Mods.None; // remove all mods from replay
+foreach (var i in mods)
+    osuReplay.Mods |= i; // add mods to replay
+osuReplay.Save(@"C:\Users\Plextora\Desktop\output.osr"); // edited osr output
+
+Console.ReadLine();
